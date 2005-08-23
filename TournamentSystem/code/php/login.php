7c57bf44452e06e13436e4776224d0101c14d1ce
Andrew Grondalski <agrondalski@gmail.com>
2005-08-23 03:06:20 +0000

    login prototype
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@27 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/login.php
+++ TournamentSystem/code/php/login.php
@@ -14,23 +14,39 @@ if ($displayLogin)
 {
 	if (!$_POST)
 	{
- 	    echo '
+	   $tourney_id = $_GET["tourney_id"];
+	   $q = mysql_query("SELECT team_id from tourney_info where tourney_id = $tourney_id");
+	   echo mysql_result($q,0,"team_id");
+	    echo '
             <FORM METHOD="POST" ACTION="login.php">
-            <B>Team</B>: DROPDOWN HERE
-            <BR>
-            <B>Password</B>: <INPUT TYPE="password" name="password">
-            <BR>
-            <INPUT TYPE="submit" value="Team Login">
+            <table cellspacing="2" cellpadding="2">
+            <TR>
+               <TD><B>Team</B>:</TD>
+               <TD>DROPDOWN HERE</TD>
+            </TR>
+            <TR></TR>
+            <TR>
+               <TD><B>Password</B>:</TD>
+               <TD><INPUT TYPE="password" name="password"></TD>
+               <TD><INPUT TYPE="submit" value="Team Login"></TD>
+            </TR>
+            </TABLE>
 	    </FORM>
 	    
             <BR><BR>
             
             <FORM METHOD="POST" ACTION="login.php">
-            <B>Admin</B>: <INPUT TYPE="text" name="admin">
-            <BR>
-            <B>Password</B>: <INPUT TYPE="password" name="password">
-            <BR>
-            <INPUT TYPE="submit" value="Admin Login">
+            <table cellspacing="2" cellpadding="2">
+            <TR>
+              <TD><B>Admin</B>:</TD>
+              <TD><INPUT TYPE="text" name="admin"></TD>
+            </TR>
+            <TR>
+              <TD><B>Password</B>:</TD> 
+              <TD><INPUT TYPE="password" name="password"></TD>
+              <TD><INPUT TYPE="submit" value="Admin Login"></TD>
+            </TR>
+            </TABLE>
             </FORM>
             ';
 	}
