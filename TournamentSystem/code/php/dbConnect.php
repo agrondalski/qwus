8f8a10a2219e974927867fa5211a523115b117d0
w d <do7hedew@gmail.com>
2005-08-25 02:18:01 +0000

    dont login as root
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@51 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/dbConnect.php
+++ TournamentSystem/code/php/dbConnect.php
@@ -1,3 +1,3 @@
-<?php $link = mysql_connect("localhost", "root", "fred")
+<?php $link = mysql_connect("localhost", "qwus", "@XantoM@")
      or die ('Could not login to mysql: ' . mysql_error() . 'Contact <a href="mailto:ultimo@quakeworld.us>ult</a> if problem persists.');
 mysql_select_db("dew") or die ('Could not select database');?>
