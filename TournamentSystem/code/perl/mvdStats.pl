0d080c42cb8e56d9fc1e37b34921bd92fb153a27
Andrew Grondalski <agrondalski@gmail.com>
2005-09-01 19:40:38 +0000

    Added player class and began implementing methods to update various player stats
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@64 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/perl/mvdStats.pl
+++ TournamentSystem/code/perl/mvdStats.pl
@@ -6,6 +6,61 @@
 # attempt to read and/or calculate final score
 # misc bs
 
+package Player;
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    $self->{NAME} = undef;
+    $self->{ROCKETFRAGS} = 0;
+    $self->{ROCKETDEATHS} = 0;
+    $self->{SHOTGUNFRAGS} = 0;
+    $self->{SHOTGUNDEATHS} = 0;
+    $self->{SSGFRAGS} = 0;
+    $self->{SSGDEATHS} = 0;
+    $self->{NAILGUNFRAGS} = 0;
+    $self->{NAILGUNDEATHS} = 0;
+    $self->{SNGFRAGS} = 0;
+    $self->{SNGDEATHS} = 0;
+    $self->{GRENADEFRAGS} = 0;
+    $self->{GRENADEDEATHS} = 0;
+    $self->{LIGHTNINGFRAGS} = 0;
+    $self->{LIGHTNINGDEATHS} = 0;
+    $self->{TEAMKILLS} = 0;
+    bless ($self, $class);
+    return $self;
+}
+
+sub name
+{
+    my $self = shift;
+    if (@_) { $self->{NAME} = shift }
+    return $self->{NAME};
+}
+
+sub rocketDeaths
+{
+    my $self = shift;
+    if (@_) { $self->{ROCKETDEATHS} = shift }
+    return $self->{ROCKETDEATHS};
+}
+
+sub rocketFrags
+{
+    my $self = shift;
+    if (@_) { $self->{ROCKETFRAGS} = shift }
+    return $self->{ROCKETFRAGS};
+}
+
+sub teamKills
+{
+    my $self = shift;
+    if (@_) { $self->{TEAMKILLS} = shift }
+    return $self->{TEAMKILLS};
+}
+
+
 foreach $mvd (@ARGV)
 {
   @strings = `strings $mvd`;
@@ -14,14 +69,17 @@ foreach $mvd (@ARGV)
   {
     if ($string =~ /(.*) rides (.*)'s rocket/)
     {
-      print "rl: " . $1 . "\t" . $2 . "\n";
+      $fraggee = findPlayer($1);
+      $fraggee->rocketDeaths($fraggee->rocketDeaths() + 1);
+      $fragger = findPlayer($2);
+      $fragger->rocketFrags($fragger->rocketFrags() + 1);
     }
     elsif ($string =~ /(.*) accepts (.*)'s shaft/)
     {
-      print "lg: " . $1 . "\t" . $2 . "\n"; 
+
     }
     elsif ($string =~ /(.*) chewed on (.*)'s boomstick/) {}
-    elsif ($string =~ /(.*) was punctured by (.*)/ {}
+    elsif ($string =~ /(.*) was punctured by (.*)/) {}
     elsif ($string =~ /(.*) was nailed by (.*)/) {}
     elsif ($string =~ /(.*) ate 2 loads of (.*)'s buckshot/) {}
     elsif ($string =~ /(.*) was brutalized by (.*)'s quad rocket/) {}
@@ -29,7 +87,46 @@ foreach $mvd (@ARGV)
     elsif ($string =~ /(.*) was gibbed by (.*)'s rocket/) {}
     elsif ($string =~ /(.*) was telefragged by his teammate/) {}
     elsif ($string =~ /(.*) was telefragged by (.*)/) {}
-    elsif ($string =~ /(.*) mows down a teammate/) {}
-    elsif ($string =~ /(.*) checks his glasses/) {}
+    elsif ($string =~ /(.*) mows down a teammate/) 
+    {
+      $fragger = findPlayer($1);
+      $fragger->teamKills($fragger->teamKills() + 1);
+    }
+    elsif ($string =~ /(.*) checks his glasses/) 
+    {
+      $fragger = findPlayer($1);
+      $fragger->teamKills($fragger->teamKills() + 1);
+    }
   }
+}
+
+outputPlayerList();
+
+sub outputPlayerList
+{
+  foreach $player (@players)
+  {
+     print 
+      $player->name() . ":\t" . 
+      $player->rocketFrags() . "\t" .
+      $player->rocketDeaths() . "\t" .
+      $player->teamKills() .
+      "\n";
+  }
+}
+
+
+// Searches player array for the name passed in
+// Returns player object if found or new player object if not
+sub findPlayer
+{
+  $playerName = shift;
+  foreach $player (@players)
+  {
+    if ($player->name() eq $playerName) { return $player }    
+  }
+  $newPlayer = Player->new();
+  $newPlayer->name($playerName);
+  push(@players, $newPlayer);
+  return $newPlayer;
 }
