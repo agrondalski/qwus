790424160617cf9f5bf568c77039eebb5030c018
skelman skelman <skelman@skelman.com>
2005-09-02 03:17:49 +0000

    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@73 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/listTourneys.php
+++ TournamentSystem/code/php/listTourneys.php
@@ -1,39 +1,18 @@
 <?php
+require 'includes.php' ;
 
-//listTourneys.php
-
-// set your infomation.
-$dbhost='localhost';
-$dbusername='skel';
-$dbuserpass='Fr3nzY!';
-$dbname='dew';
-
-$tid = $_REQUEST['tourney_id'];
-
-// Connecting, selecting database
-$link = mysql_connect($dbhost, $dbusername, $dbuserpass)
-   or die('Could not connect: ' . mysql_error());
-mysql_select_db($dbname) or die('Could not select database');
-
-// Performing SQL query
-$query = 'SELECT * FROM tourney';
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+$tourneys = tourney::getAllTourneys();
 
 // Printing results in HTML
 echo "<table border=1 cellpadding=2 cellspacing=0>\n";
 echo "<th>tourney_id</th><th>Tourney Name</th>";
-while ($line = mysql_fetch_array($result)) {
-   echo "\t<tr>\n";
-   echo "\t<td>",$line['tourney_id'],"</td>\n";
-   echo "\t<td><a href='?a=tourneyHome&tourney_id=",$line['tourney_id'],"'>",$line['name'],"</a></td>\n";
-   echo "\t</tr>\n";
+foreach ($tourneys as $t) {
+  echo "\t<tr>\n";
+  echo "\t<td>",$t->getValue('tourney_id'),"</td>\n";
+  echo "\t<td><a href='tourneyHome.php?tourney_id=",$t->getValue('tourney_id'),"'>",$t->getValue('name'),"</a></td>\n";
+  echo "\t</tr>\n";
+
 }
 echo "</table>\n";
-
-// Free resultset
-mysql_free_result($result);
-
-// Closing connection
-mysql_close($link);
 
 ?>
