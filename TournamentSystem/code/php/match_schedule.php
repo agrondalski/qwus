cc09c82f9e1b28f45092ea527f35581a4eed72f8
w d <do7hedew@gmail.com>
2005-09-12 04:20:58 +0000

    add match_schedule
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@156 25caed32-bbfe-0310-b14d-ee6f34c715aa

---
+++ TournamentSystem/code/php/match_schedule.php
@@ -0,0 +1,167 @@
+<?php
+require_once 'dbConnect.php' ;
+?>
+
+<?php
+class match_schedule
+{
+  private $schedule_id ;
+  private $division_id ;
+  private $name ;
+  private $deadline ;
+
+  function __construct($a)
+    {
+      $id = $a['schedule_id'] ;
+
+      if (isset($id) && is_numeric($id))
+	{
+	  $this->schedule_id = $id ;
+
+	  if ($this->getMatchInfo()==util::NOTFOUND)
+	    {
+	      util::throwException("No match exists with specified id");
+	    }
+	  else
+	    {
+	      return ;
+	    }
+	}
+
+      foreach($this as $key => $value)
+	{
+	  $this->$key = $this->validateColumn($a[$key], $key, true) ;
+	}
+
+      $sql_str = sprintf("insert into match_schedule(division_id, name, deadline)" .
+                         "values(%d, '%s', '%s')",
+                         $this->division_id, $this->name, $this->deadline) ;
+
+      $result = mysql_query($sql_str) or util::throwSQLException("Unable to execute : $sql_str " . $mysql_error) ;
+      $this->schedule_id = mysql_insert_id() ;
+    }
+
+  private function getMatchInfo()
+    {
+      $sql_str = sprintf("select division_id, name, deadline from match_table where schedule_id=%d", $this->schedule_id) ;
+      $result  = mysql_query($sql_str) or util::throwSQLException("Unable to execute : $sql_str : " . mysql_error());
+
+      if (mysql_num_rows($result)!=1)
+	{
+	  mysql_free_result($result) ;
+	  return util::NOTFOUND ;
+	}
+      $row = mysql_fetch_row($result) ;
+
+      $this->schedule_id  = $row[0] ;
+      $this->name         = $row[2] ;
+      $this->deadline     = $row[2] ;
+
+      mysql_free_result($result) ;
+
+      return util::FOUND ;
+    }
+
+  public function validateColumnName($col)
+    {
+      $found ;
+      foreach($this as $key => $value)
+	{
+	  if ($col === $key)
+	    {
+	      return ;
+	    }
+	}
+
+      util::throwException('invalid column name specified') ;
+    }
+
+  public static function validateColumn($val, $col, $cons=false)
+    {
+      if ($col == 'schedule_id')
+	{
+	  if (!$cons)
+	    {
+	      if (util::isNull($val))
+		{
+		  util::throwException($col . ' cannot be null') ;
+		}
+
+	      return util::mysql_real_escape_string($val) ;
+	    }
+	}
+
+      elseif ($col == 'division_id')
+	{
+ 	  if (util::isNull($val))
+	    {
+	      util::throwException($col . ' cannot be null') ;
+	    }
+
+	  return util::mysql_real_escape_string($val) ;
+	}
+
+      elseif ($col == 'name')
+	{
+ 	  if (util::isNull($val))
+	    {
+	      util::throwException($col . ' cannot be null') ;
+	    }
+
+	  return util::mysql_real_escape_string($val) ;
+	}
+
+      elseif ($col == 'deadline')
+	{
+	  /*
+ 	  if (util::isNull($val))
+	    {
+	      util::throwException($col . ' cannot be null') ;
+	    }
+	  */
+
+	  if (!util::isNull($val) && !util::isValidDate($val))
+	    {
+	      util::throwException('invalid date specified for ' . $col) ;
+	    }
+
+	  return util::nvl(util::mysql_real_escape_string($val), util::DEFAULT_DATE) ;
+	}
+
+      else
+	{
+	  util::throwException('invalid column specified') ;
+	}
+    }
+
+  public function getValue($col)
+    {
+      $this->validateColumnName($col) ;
+      return htmlentities($this->$col) ;
+    }
+
+  public function update($col, $val)
+    {
+      $this->$col = $this->validateColumn($val, $col) ;
+
+      if (is_numeric($this->$col))
+	{
+	  $sql_str = sprintf("update match_table set %s=%d where schedule_id=%d", $col, $this->$col, $this->schedule_id) ;
+	}
+      else
+	{
+	  $sql_str = sprintf("update match_table set %s='%s' where schedule_id=%d", $col, $this->$col, $this->schedule_id) ;
+	}
+
+      $result  = mysql_query($sql_str) or util::throwSQLException("Unable to execute : $sql_str : " . mysql_error());
+      mysql_free_result($result) ;
+    }
+
+  public function delete()
+    {
+      $sql_str = sprintf("delete from match_table where schedule_id=%d", $this->schedule_id) ;
+      $result  = mysql_query($sql_str) or util::throwSQLException("Unable to execute : $sql_str : " . mysql_error());      
+      mysql_free_result($result) ;
+    }
+}
+?>
