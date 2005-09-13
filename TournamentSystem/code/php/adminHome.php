a95cd83022001210f2fa164670361559064f2845
skelman skelman <skelman@skelman.com>
2005-09-13 03:10:18 +0000

    html cleanup and cosmetic changes to admin/tourney home pages
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@172 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/adminHome.php
+++ TournamentSystem/code/php/adminHome.php
@@ -5,7 +5,7 @@ require_once 'login.php';
 echo "<b>Actions</b><br>";
 echo "<table border=1 cellpadding=2 cellspacing=0>";
 echo "<tr>";
-echo "<td><font size='2'><a href='?a=listTourneys'>Manage Tourney</a></td>";
+echo "<td><a href='?a=listTourneys'>Select Tourney</a></td>";
 echo "<td>&nbsp;</td>";
 echo "</tr>";
 
@@ -15,28 +15,28 @@ try
   if ($p->hasColumn())
     {
       echo "<tr>";
-      echo "<td><font size='2'><a href='?a=manageColumn'>Create Column</a></td>";
-      echo "<td><font size='2'><a href='?a=listColumn'>Manage Column</a></td>";
+      echo "<td><a href='?a=manageColumn'>Create Column</a></td>";
+      echo "<td><a href='?a=listColumn'>Manage Column</a></td>";
       echo "</tr>";
     }
 
   if ($p->isSuperAdmin())
     {
       echo "<tr>";
-      echo "<td><font size='2'><a href='?a=manageNews'>Create News</a></td>";
-      echo "<td><font size='2'><a href='?a=listNews'>Manage News</a></td>";
+      echo "<td><a href='?a=manageNews'>Create News</a></td>";
+      echo "<td><a href='?a=listNews'>Manage News</a></td>";
       echo "</tr>";
       echo "<tr>";
-      echo "<td><font size='2'><a href='?a=manageTeam'>Create a Team</a></td>";
-      echo "<td><font size='2'><a href='?a=listTeams'>Manage Teams</a></td>";
+      echo "<td><a href='?a=manageTeam'>Create a Team</a></td>";
+      echo "<td><a href='?a=listTeams'>Manage Teams</a></td>";
       echo "</tr>";
       echo "<tr>";
-      echo "<td><font size='2'><a href='?a=managePlayer'>Create a Player</a></td>";
-      echo "<td><font size='2'><a href='?a=listPlayers'>Manage Players</a></td>";
+      echo "<td><a href='?a=managePlayer'>Create a Player</a></td>";
+      echo "<td><a href='?a=listPlayers'>Manage Players</a></td>";
       echo "</tr>";
       echo "<tr>";
       echo "<td>&nbsp;</td>";
-      echo "<td><font size='2'><a href='?a=listMaps'>List Maps</a></td>";
+      echo "<td><a href='?a=listMaps'>List Maps</a></td>";
       echo "</tr>";
     }
 }
