3f15ce1761f2f4a49e790b04f2bc9444d29fd40c
w d <do7hedew@gmail.com>
2005-09-16 02:17:45 +0000

    switch back to dew db
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@201 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/dbConnect.php
+++ TournamentSystem/code/php/dbConnect.php
@@ -2,5 +2,5 @@
 $link = mysql_connect("localhost", "qwus", "@XantoM@")
      or util::throwException('Could not login to mysql: ' . mysql_error() . 'Contact <a href="mailto:ultimo@quakeworld.us>ult</a> if problem persists.');
 
-     mysql_select_db("dew_test", $link) or util::throwException('Cound not select database') ;
+     mysql_select_db("dew", $link) or util::throwException('Cound not select database') ;
 ;?>
