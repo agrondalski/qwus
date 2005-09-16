c14f20daa2d7bcad73dbab377b4417b4e4e01436
w d <do7hedew@gmail.com>
2005-09-16 02:43:30 +0000

    new path for classes
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@206 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/includes.php
+++ TournamentSystem/code/php/includes.php
@@ -4,24 +4,21 @@ function __autoload($class_name) {
    require_once $class_name . '.php';
 }
 */
-/*
 require_once 'dbConnect.php' ;
-require_once 'comments.php' ;
-require_once 'division.php' ;
-require_once 'game.php' ;
-require_once 'game_type.php' ;
-require_once 'location.php' ;
-
-require_once 'log_entry.php' ;
-require_once 'map.php' ;
-require_once 'match.php' ;
-require_once 'match_schedule.php' ;
-require_once 'news.php' ;
-require_once 'player.php' ;
-require_once 'poll.php' ;
-require_once 'stats.php' ;
-*/
-//require_once 'team.php' ;
-//require_once 'tourney.php' ;
-//require_once 'util.php' ;
+require_once 'classes/comments.php' ;
+require_once 'classes/division.php' ;
+require_once 'classes/game.php' ;
+require_once 'classes/game_type.php' ;
+require_once 'classes/location.php' ;
+require_once 'classes/log_entry.php' ;
+require_once 'classes/map.php' ;
+require_once 'classes/match.php' ;
+require_once 'classes/match_schedule.php' ;
+require_once 'classes/news.php' ;
+require_once 'classes/player.php' ;
+require_once 'classes/poll.php' ;
+require_once 'classes/stats.php' ;
+require_once 'classes/team.php' ;
+require_once 'classes/tourney.php' ;
+require_once 'classes/util.php' ;
 ?>
