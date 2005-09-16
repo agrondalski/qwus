80eabf10f161261fde2b286c8dd24e9b681aed2d
w d <do7hedew@gmail.com>
2005-09-16 02:27:55 +0000

    moved util file
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@205 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/includes.php
+++ TournamentSystem/code/php/includes.php
@@ -4,13 +4,14 @@ function __autoload($class_name) {
    require_once $class_name . '.php';
 }
 */
-
+/*
 require_once 'dbConnect.php' ;
 require_once 'comments.php' ;
 require_once 'division.php' ;
 require_once 'game.php' ;
 require_once 'game_type.php' ;
 require_once 'location.php' ;
+
 require_once 'log_entry.php' ;
 require_once 'map.php' ;
 require_once 'match.php' ;
@@ -19,7 +20,8 @@ require_once 'news.php' ;
 require_once 'player.php' ;
 require_once 'poll.php' ;
 require_once 'stats.php' ;
-require_once 'team.php' ;
-require_once 'tourney.php' ;
-require_once 'util.php' ;
+*/
+//require_once 'team.php' ;
+//require_once 'tourney.php' ;
+//require_once 'util.php' ;
 ?>
