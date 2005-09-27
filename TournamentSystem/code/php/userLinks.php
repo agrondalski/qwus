2f5c0adbc69ed219938317cf05180f6c609b6b95
skelman skelman <skelman@skelman.com>
2005-09-27 05:07:45 +0000

    Added rules link and file (in wrong place)
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@286 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/userLinks.php
+++ TournamentSystem/code/php/userLinks.php
@@ -8,7 +8,8 @@ if ($tid != "")
 	echo "&nbsp;<a href='?a=standings&tourney_id=",$tid,"'>Standings</a>&nbsp;-";
 	echo "&nbsp;<a href='?a=schedule&tourney_id=",$tid,"'>Schedule</a>&nbsp;-";
 	echo "&nbsp;<a href='?a=statistics&tourney_id=",$tid,"'>Stats</a>&nbsp;-";
-	echo "&nbsp;<a href='?a=results&tourney_id=",$tid,"'>Results</a>";
+	echo "&nbsp;<a href='?a=results&tourney_id=",$tid,"'>Results</a>&nbsp;-";
+	echo "&nbsp;<a href='nanqr2rules.html' target='_blank'>Rules</a>";
 	echo "<br>";
 }
 ?>
