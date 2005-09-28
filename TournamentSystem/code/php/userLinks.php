0f120e98338cb1f805afdc629dec81ae3c58a30b
w d <do7hedew@gmail.com>
2005-09-28 02:52:34 +0000

    fix rules link
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@294 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/userLinks.php
+++ TournamentSystem/code/php/userLinks.php
@@ -4,12 +4,12 @@ $tid = $_REQUEST['tourney_id'];
 
 if ($tid != "") 
 {
-	echo "<a href='?a=home&tourney_id=",$tid,"'>News</a>&nbsp;-";
-	echo "&nbsp;<a href='?a=standings&tourney_id=",$tid,"'>Standings</a>&nbsp;-";
-	echo "&nbsp;<a href='?a=schedule&tourney_id=",$tid,"'>Schedule</a>&nbsp;-";
-	echo "&nbsp;<a href='?a=statistics&tourney_id=",$tid,"'>Stats</a>&nbsp;-";
-	echo "&nbsp;<a href='?a=results&tourney_id=",$tid,"'>Results</a>&nbsp;-";
-	echo "&nbsp;<a href='nanqr2rules.html' target='_blank'>Rules</a>";
-	echo "<br>";
+  echo "<a href='?a=home&amp;tourney_id=" . $tid . "'>News</a>&nbsp;-";
+  echo "&nbsp;<a href='?a=standings&amp;tourney_id=" . $tid . "'>Standings</a>&nbsp;-";
+  echo "&nbsp;<a href='?a=schedule&amp;tourney_id=" . $tid . "'>Schedule</a>&nbsp;-";
+  echo "&nbsp;<a href='?a=statistics&amp;tourney_id=" . $tid . "'>Stats</a>&nbsp;-";
+  echo "&nbsp;<a href='?a=results&amp;tourney_id=" . $tid . "'>Results</a>&nbsp;-";
+  echo "&nbsp;<a href='?a=nanqr2rules&amp;tourney_id=" . $tid . "'>Rules</a>&nbsp;";
+  echo "<br>";
 }
 ?>
