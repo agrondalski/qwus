b3833153a9bfed44f40b2bb3e899d63b40abb584
w d <do7hedew@gmail.com>
2005-10-03 23:59:06 +0000

    new user for test env
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@325 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/dbConnect.php
+++ TournamentSystem/code/php/dbConnect.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysql_connect("localhost", "qwus", "@XantoM@")
+$link = mysql_connect("localhost", "test_user", "8s6T1OL")
      or util::throwException('Could not login to mysql: ' . mysql_error() . 'Contact <a href="mailto:ultimo@quakeworld.us>ult</a> if problem persists.');
 
      mysql_select_db("dew", $link) or util::throwException('Cound not select database') ;
