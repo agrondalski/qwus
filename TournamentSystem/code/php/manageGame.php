a544311c668c718fbae0be9220b40b05cb9f0d08
skelman skelman <skelman@skelman.com>
2005-10-09 05:17:45 +0000

    Initial version of statCreation page, unable to test manageGame.php has stats listed that are from current game.  Need to add 'edit' and 'delete' links and also the edit page, and savepage for stats.
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@366 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/php/manageGame.php
+++ TournamentSystem/code/php/manageGame.php
@@ -86,54 +86,42 @@ try
   echo "</tr>";  
   
   
-	if ($mode == "edit")
+	
+  echo "<tr><td colspan=2><input type='submit' value='Submit' name='B1' class='button'>";
+  echo "&nbsp;<input type='reset' value='Reset' name='B2' class='button'></td></tr></table>";
+  echo "</p></font>";
+
+  echo "</form>" ;
+  
+  if ($mode == "edit")
 	{
-	    $game_id = $_REQUEST['game_id'];
-        $g = new game(array('game_id'=>$game_id));
-		$i = 1;
-		echo "a";
-		foreach ($g->getStats() as $p1) 
-		{	echo "a";
-		    try {
-			    //$p1 = new player(array('player_id'=>$tmp->getValue('player_id')));
-			}
-			catch (Exception $e) {}
-			//if ($p1->getValue('player_id') == $m->getValue('team1_id')) 
-			//{
-			    echo "a";
-				echo "<tr><td><select name='t1p",$i,"'>";
-				$sel = "";
-				if ($p1->getValue('player_id') == $t1p[$i]) 
-				{
-					$sel = "selected";
-				}
-				echo "a";
-				echo "<option value='",$p1->getValue('name'),"' ",$sel,">",$p1->getValue('player_id');
-				echo "</select></td>";
-				echo "<td>",$g->getValue('stat_name'),":&nbsp;";
-				echo "<input type='text' name't1p",$i,"' value='",$g->getValue('value'),"' size=</td></tr>";
-				$i++;
-			//}
+		echo "<br><b>Stats from current game:</b>";
+		$game_id = $_REQUEST['game_id'];
+		$g = new game(array('game_id'=>$game_id));
+		echo "<table border=1 cellpadding=2 cellspacing=0>";
+		echo "<tr><th>Team</th><th>Player</th><th>Stat</th><th>Value</th></tr>";
+		foreach ($g->getStats(array('team_id', SORT_DESC, 'value', SORT_DESC)) as $s) 
+		{	
+			//echo "a";
+			$t = new team(array('team_id'=>$s->getValue('team_id')));
+			$p = new player(array('player_id'=>$s->getValue('player_id')));
+			//echo "a";
+			echo "<tr>";
+			echo "<td>",$t->getValue('name_abbr'),"</td>";
+			echo "<td>",$p->getValue('name'),"</td>";
+			echo "<td>",$s->getValue('stat_name'),"</td>";
+			echo "<td>",$s->getValue('value'),"</td>";
+			echo "</tr>";			
 		}
+		echo "</table>";
 
 	}
 	else
 	{
 
 	}
-  //$pc = 1;
-  //$i  = 1;
-  //for ($i = 1;$i <= 10;$i++;)
-  //{
-  //echo "<td><input type='text' name't1p",$i,"' value='",$t1p[$i],"' size=
-  
-  //}
-  echo "<tr><td colspan=2><input type='submit' value='Submit' name='B1' class='button'>";
-  echo "&nbsp;<input type='reset' value='Reset' name='B2' class='button'></td></tr></table>";
-  echo "</p></font>";
-
-  echo "</form>" ;
-
+	echo "<hr>";
+	echo "<br><b>Current Games:</b>";
   include 'listGames.php';
 }
 catch (Exception $e) {}
