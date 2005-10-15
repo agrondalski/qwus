1a50d293c39354fa64d7f32c920b80cd0da8ecd2
Andrew Grondalski <agrondalski@gmail.com>
2005-10-15 23:37:53 +0000

    duh
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@493 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/perl/updateToSvn.pl
+++ TournamentSystem/code/perl/updateToSvn.pl
@@ -7,18 +7,13 @@ my $filename = "SiteBackup-$date.tar";
 my $htmlHome = "/var/www/html/";
 @backupDirectories = qw(php perl css);
 
-foreach $dir (@backupDirectories)
-{
-    print "$dir\n";
-}
-
 $shell = `tar -cf "$filename" /var/www/html/php /var/www/html/perl /var/www/html/css /var/www/html/*.php`;
 
 $shell = `gzip -9 "$filename"`;
 
 # copy the new stuff over.. (careful not to copy hidden svn files)
 
-$shell = `mv /var/www/html/php/dBConnect.php /var/www/html/oldDbConnect.php`;
+$shell = `mv /var/www/html/php/dBConnect.php /var/www/html/php/oldDbConnect.php`;
 
 $shell = `cp -f /usr/www/svn/TournamentSystem/code/php/classes/*.php /var/www/html/php/classes`;
 
