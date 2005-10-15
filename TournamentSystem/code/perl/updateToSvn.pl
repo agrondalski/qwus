39247c5729a5b29f8de57592ae2ac58b03b36741
Andrew Grondalski <agrondalski@gmail.com>
2005-10-15 23:34:46 +0000

    updated updated script
    git-svn-id: http://localhost:8080/svn/qwus/qwus/trunk@492 25caed32-bbfe-0310-b14d-ee6f34c715aa

--- TournamentSystem/code/perl/updateToSvn.pl
+++ TournamentSystem/code/perl/updateToSvn.pl
@@ -14,13 +14,16 @@ foreach $dir (@backupDirectories)
 
 $shell = `tar -cf "$filename" /var/www/html/php /var/www/html/perl /var/www/html/css /var/www/html/*.php`;
 
-#$shell = `gzip -9 "$filename"`;
+$shell = `gzip -9 "$filename"`;
 
 # copy the new stuff over.. (careful not to copy hidden svn files)
 
-#$shell = `cp -f /usr/www/svn/TournamentSystem/code/php/classes/*.php /var/www/html/php/classes`;
+$shell = `mv /var/www/html/php/dBConnect.php /var/www/html/oldDbConnect.php`;
 
-#$shell = `cp -f /usr/www/svn/TournamentSystem/code/php/*.php /var/www/html/php`;
+$shell = `cp -f /usr/www/svn/TournamentSystem/code/php/classes/*.php /var/www/html/php/classes`;
 
-#$shell = `cp -f /usr/www/svn/TournamentSystem/code/perl/*.pl /var/www/html/perl`;
+$shell = `cp -f /usr/www/svn/TournamentSystem/code/php/*.php /var/www/html/php`;
 
+$shell = `cp -f /usr/www/svn/TournamentSystem/code/perl/*.pl /var/www/html/perl`;
+
+$shell = `mv /var/www/html/php/oldDbConnect.php /var/www/html/php/dbConnect.php`;
