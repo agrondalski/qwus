b4e3b3a22cbb8d07a42cdef3fa40b346bd25a56f
agrondalski <agrondalski@gmail.com>
2020-04-04 16:41:59 +0000

    database connection

--- TournamentSystem/code/php/dbConnect.php
+++ TournamentSystem/code/php/dbConnect.php
@@ -1,6 +1,6 @@
 <?php
-$link = mysql_connect("localhost", "test_user", "8s6T1OL")
-     or util::throwException('Could not login to mysql: ' . mysql_error() . 'Contact <a href="mailto:ultimo@quakeworld.us>ult</a> if problem persists.');
+$link = mysqli_connect("localhost", "test_user", "password")
+     or util::throwException('Could not login to mysql: ' . mysqli_error($link) . 'Contact <a href="mailto:ultimo@quakeworld.us>ult</a> if problem persists.');
 
-     mysql_select_db("dew", $link) or util::throwException('Cound not select database') ;
+     mysqli_select_db($link, "dew") or util::throwException('Cound not select database') ;
 ;?>
